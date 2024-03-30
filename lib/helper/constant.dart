fb629e265b5aaf0f8ba7a1444cbad573aacbe431
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-30 21:41:53 +0700

    add icon assets

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 class TokoOnlineKit {
   static const String splash = '/uikit_tokoonline_splash';
   static const String onboarding = '/uikit_tokoonline_onboarding';
@@ -8,6 +11,41 @@ class TokoOnlineKit {
   static const String profile = '';
 }
 
+Future showToast(
+    {required String msg, Color? backgroundColor, Color? textColor}) {
+  return Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    fontSize: 16,
+    gravity: ToastGravity.CENTER,
+    backgroundColor: backgroundColor,
+    textColor: textColor ?? Colors.white,
+  );
+}
+
+class Const {
+  static const double margin = 18;
+  static const double radius = 12;
+  static const double space5 = 5;
+  static const double space8 = 8;
+  static const double space12 = 12;
+  static const double space15 = 15;
+  static const double space25 = 25;
+}
+
 class Assets {
   static const String logo = 'assets/bellcommerce_logo.png';
+}
+
+class Illustrations {
+  static const String forgotPassword =
+      'assets/illustrations/bellcommerce_forgot_password.svg';
+  static const String forgotPasswordSuccess =
+      'assets/illustrations/bellcommerce_forgot_password_success.svg';
+  static const String onBoarding1 =
+      'assets/illustrations/bellcommerce_on_boarding_1.png';
+  static const String onBoarding2 =
+      'assets/illustrations/bellcommerce_on_boarding_2.png';
+  static const String onBoarding3 =
+      'assets/illustrations/bellcommerce_on_boarding_3.png';
 }
