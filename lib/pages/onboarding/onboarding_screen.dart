b2f08f76b343dfbb69a6a4107e23aee9aa73ceb5
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-30 21:52:27 +0700

    applocaliztion

--- lib/pages/onboarding/onboarding_screen.dart
+++ lib/pages/onboarding/onboarding_screen.dart
@@ -17,6 +17,27 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     _pageController = PageController();
   }
 
+  List<OnBoardingModel> _onboardingPageList(BuildContext context) => [
+        OnBoardingModel(
+          title: AppLocalizations.of(context)!.product,
+          subtitle: AppLocalizations.of(context)!
+              .select_different_products_the_freedom_is_yours,
+          image: Illustrations.onBoarding1,
+        ),
+        OnBoardingModel(
+          title: AppLocalizations.of(context)!.easy,
+          subtitle: AppLocalizations.of(context)!
+              .pay_for_the_products_you_buy_safely_and_easly,
+          image: Illustrations.onBoarding2,
+        ),
+        OnBoardingModel(
+          title: AppLocalizations.of(context)!.fast,
+          subtitle: AppLocalizations.of(context)!
+              .super_fast_delivery_right_at_your_door,
+          image: Illustrations.onBoarding3,
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
