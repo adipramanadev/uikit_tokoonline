aad0aeb70145e6b8ec1f80ea4cdab3861a13b9c1
Adipramana <herryprasetyo.dev@gmail.com>
2024-03-30 22:29:07 +0700

    updarte buildFooter

--- lib/pages/onboarding/widgets/buildFooter.dart
+++ lib/pages/onboarding/widgets/buildFooter.dart
@@ -1,10 +1,20 @@
 part of '../onboardingpage.dart';
 
 class BuildFooter extends StatelessWidget {
-  const BuildFooter({super.key});
+  final int index;
+  final int dotsCount;
+  final VoidCallback onTap;
+
+  const BuildFooter({
+    Key? key,
+    required this.index,
+    required this.dotsCount,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _theme = Theme.of(context);
     return const Placeholder();
   }
 }
